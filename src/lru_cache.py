7b8fc70ef7dd7652bafde443e6feb679a2d460bb
HPavlova <77845327+HPavlova@users.noreply.github.com>
2022-10-05 22:21:25 +0300

    Create lru_cache.py

---
+++ src/lru_cache.py
@@ -0,0 +1,30 @@
+from src.redis_client import redis_client
+from redis_lru import RedisLRU
+import json
+
+cache = RedisLRU(redis_client)
+
+redis_client.flushdb()
+
+def show_cache():
+    for key in redis_client.scan_iter():
+        print('Key:', key)
+        print('Value:', redis_client.get(key))
+
+class LruCache:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwds):
+        key = args[0]
+        if cache.get(key) is not None:
+            value_json = cache.get(key)
+            value = json.loads(value_json)
+            print(value)
+            show_cache()
+        else:
+            value=self.func(*args, **kwds)
+            value_json = json.dumps(value)
+            cache.set(key, value_json)
+            print(value)
+            show_cache()
